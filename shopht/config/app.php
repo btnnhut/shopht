812199f1220d31d0a13bd9f20f81b6224accbbd7
btnnhut <btnnhut@gmail.com>
2020-08-21 20:50:33 +0700

    install Laravel: intervention, file manager, ckeditor, laravelcollective

--- shopht/config/app.php
+++ shopht/config/app.php
@@ -179,6 +179,7 @@ return [
         UniSharp\LaravelFilemanager\LaravelFilemanagerServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
         Unisharp\Ckeditor\ServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -229,6 +230,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Image' => Intervention\Image\Facades\Image::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
